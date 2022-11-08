9be3c0d08b0fba3e315ff4f6d6bec8d20903b01e
coderLixiang <2524447983@qq.com>
2022-11-08 09:19:15 +0800

    求和案例vuex的实现

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>当前求和为：{{ sum }}</h1>
+    <h1>当前求和为：{{ $store.state.sum }}</h1>
     <select v-model.number="n">
       <option value="1">1</option>
       <option value="2">2</option>
@@ -19,25 +19,20 @@ export default {
   data() {
     return {
       n: 1, //用户选择的数字
-      sum: 0, //当前的和
     };
   },
   methods: {
     increment() {
-      this.sum += this.n;
+      this.$store.commit("JIA", this.n);
     },
     decrement() {
-      this.sum -= this.n;
+      this.$store.commit("JIAN", this.n);
     },
     incrementOdd() {
-      if (this.sum % 2 == 1) {
-        this.sum += this.n;
-      }
+      this.$store.dispatch("jiaOdd", this.n);
     },
     incrementWait() {
-      setTimeout(() => {
-        this.sum += this.n;
-      }, 1000);
+      this.$store.dispatch("jiaWait",this.n)
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,36 @@ import Vue from "vue"
 Vue.use(Vuex)
 //准备actions --用于响应组件中的动作
 const actions = {
-
+  // jia(context, value) {
+  //   context.commit("JIA", value)
+  // },
+  jiaOdd(context, value) {
+    if (context.state.sum%2==1) {
+      context.commit("JIA", value)
+    }
+  },
+  jiaWait(context, value) {
+    setTimeout(() => {
+      context.commit("JIA", value)
+    }, 1000);
+  },
+  // jian(context, value) {
+  //   context.commit("JIAN", value)
+  // }
 }
 //准备mutations --用于操作数据state
 const mutations = {
+  JIA(state, value) {
+    state.sum += value
+  },
+  JIAN(state, value) {
+    state.sum -= value
+  },
 
 }
 //准备state ,用于存储数据
 const state = {
-
+  sum: 0//当前的和
 }
 //创建并暴露store
 export default new Vuex.Store({
